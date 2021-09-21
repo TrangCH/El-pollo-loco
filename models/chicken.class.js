2b3bb1da522b02219c0af941beb8f960f5cc29fa
TrangCH <camtrang@web.de>
2021-09-21 17:56:01 +0200

    Place the chicken at random

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -5,7 +5,11 @@ class Chicken extends MovableObject {
     * This function is always there. In every class. It is always called first of all.
     */
     constructor() {
+        // super(), nur bei Methoden in übergeordneten Klassen
         super().loadImage('img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png');
+    
+        this.x = 200 + Math.random() * 500; //  Math.random() liegt zwischen 0 und 1, Zahl zwischen 200 und 700
+
     }
 
 
