6d2b15e60f839b3ba2de187fd9bddfb41dd1f676
TrangCH <camtrang@web.de>
2021-09-27 18:57:21 +0200

    imageCache

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,15 +4,30 @@ class MovableObject { // template
     img;
     width = 100;
     height = 150;
+    imageCache = {}; // Bilderspeicher JSON
 
     /**
-     * This function loads the images.
+     * This function loads the image.
      * @param {string} path source
      */
     // loadImage('img/teyt.png');
     loadImage(path) {          // Image already exists. Image only in javascript.
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+
+    /**
+     * This function loads the images.
+     * @param {array} arr  = ['img/image1.png, 'img/image2.png', ...]
+     */
+    loadImages(arr) { // array
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path; // Laden wir das Bild in dieses Image-Objekt hinein.
+            this.imageCache[path] = path; // siehe imageCache oben // update imageCache
+        });
+
     }
 
 
