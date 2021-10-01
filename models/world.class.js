155347276443dc7c4b0aaea93e487545d89e074d
TrangCH <camtrang@web.de>
2021-10-01 18:42:12 +0200

    Move camera, insert background objects, optimization

--- models/world.class.js
+++ models/world.class.js
@@ -11,6 +11,11 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        // -1.
+        new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719), // constructor(imagePath, x, y)
+        new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', -719), // constructor(imagePath, x, y)
+        new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', -719), // constructor(imagePath, x, y)
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719), // constructor(imagePath, x, y)
         // 1.
         new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0), // constructor(imagePath, x, y)
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/1.png', 0), // constructor(imagePath, x, y)
